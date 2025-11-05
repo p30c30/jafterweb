f4ebc638359792b3d17b7b52e1f256844297b217
p30c30 <p30c30@gmail.com>
2025-11-05 18:54:50 +0100

    Update script.js tv1.1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ===================================================================
 // ==        SCRIPT36.JS - VERSIÓN COMPLETA (v36.7)               ==
 // ===================================================================
-console.log('✅ script.js v37.0 CARGADO');
+console.log('✅ script.js v37.1 CARGADO');
 
 // ===== Estado global =====
 let currentSeccion = null, currentFotoIndex = 0, todasLasFotos = [], carruselActualIndex = 0, carruselFotos = [], datosGlobales = null, isModalOpen = false;
