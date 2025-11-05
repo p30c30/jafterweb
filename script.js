fb5ccbf30bd9574489f02e138313df7b16d5b1e0
p30c30 <p30c30@gmail.com>
2025-11-05 03:35:13 +0100

    Update and rename script36.js to script.js v.8

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ===================================================================
 // ==        SCRIPT36.JS - VERSIÓN COMPLETA (v36.7)               ==
 // ===================================================================
-console.log('✅ script36.js v36.7 CARGADO');
+console.log('✅ script.js v36.8 CARGADO');
 
 // ===== Estado global =====
 let currentSeccion = null, currentFotoIndex = 0, todasLasFotos = [], carruselActualIndex = 0, carruselFotos = [], datosGlobales = null, isModalOpen = false;
